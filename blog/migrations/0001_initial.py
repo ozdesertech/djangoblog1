75c7dd2aaef16ce02c63f2029afd363e695836d4
root <techozdeser@gmail.com>
2018-06-25 02:55:44 +0300

    MOdels have been created for our blog application

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Author',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+                ('email', models.EmailField(unique=True, max_length=254)),
+                ('bio', models.TextField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('cat_name', models.CharField(max_length=50)),
+                ('cat_description', models.CharField(max_length=255)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=200)),
+                ('body', models.TextField()),
+                ('created_date', models.DateTimeField(auto_now_add=True)),
+                ('updated_date', models.DateTimeField(auto_now=True)),
+                ('author', models.ForeignKey(to='blog.Author')),
+                ('categories', models.ManyToManyField(to='blog.Category')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Tag',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('tag_name', models.CharField(max_length=50)),
+                ('tag_description', models.CharField(max_length=255)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='post',
+            name='tags',
+            field=models.ManyToManyField(to='blog.Tag'),
+        ),
+    ]
