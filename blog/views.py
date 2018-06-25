30f86ecbfb59dad6911c49772988f4c6a6a8ea8f
root <techozdeser@gmail.com>
2018-06-25 04:15:11 +0300

    .gitignore file is added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    data = {'mydata' : "HELLO WORLD"}
+    return render(request, 'blog/index.html', data)
